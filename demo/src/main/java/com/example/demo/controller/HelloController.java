0f009462339a923419a099bcf0722c5ca1c603f4
NoThingNoDust <github666>
2021-04-29 14:48:05 +0800

    add

--- demo/src/main/java/com/example/demo/controller/HelloController.java
+++ demo/src/main/java/com/example/demo/controller/HelloController.java
@@ -5,6 +5,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
+import java.io.File;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 
@@ -18,6 +23,24 @@ public class HelloController {
     public String hello() throws UnknownHostException, InterruptedException {
         helloService.testAAA();
         return InetAddress.getLocalHost().getHostName();
+    }
+
+
+    @GetMapping("compiler")
+    public String compiler() {
+        //获取系统Java编译器
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        //获取Java文件管理器
+        StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null);
+        //定义要编译的源文件
+        File file = new File("/Users/liule/Documents/HelloController.java");
+        //通过源文件获取到要编译的Java类源码迭代器，包括所有内部类，其中每个类都是一个 JavaFileObject，也被称为一个汇编单元
+        Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjects(file);
+        //生成编译任务
+        JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, null, null, compilationUnits);
+        //执行编译任务
+        task.call();
+        return "OK";
     }
 
 }
